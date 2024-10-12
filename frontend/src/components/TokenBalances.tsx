1a58c94a693ba44eed13b46b86f1856a78f66740
wr1159 <chuweiron18@gmail.com>
2024-10-12 22:38:33 +0800

    Return nothing if balance is not available

--- frontend/src/components/TokenBalances.tsx
+++ frontend/src/components/TokenBalances.tsx
@@ -2,6 +2,7 @@ import { useReadContracts } from "wagmi";
 import { tokenOptions, XSGD_ABI } from "src/constants";
 import { TokenRow } from "@coinbase/onchainkit/token";
 import { formatUnits } from "ethers/lib/utils";
+import { BigNumberish } from "ethers";
 
 export default function TokenBalances({
     walletAddress,
@@ -16,24 +17,27 @@ export default function TokenBalances({
             args: [walletAddress],
         })),
     });
-
     return (
         <>
             <div className="font-normal text-indigo-600 text-3xl not-italic tracking-[-1.2px] mb-8">
                 Wallet Balance
             </div>
             {balances &&
-                tokenOptions.map((token, index) => (
-                    <TokenRow
-                        token={token}
-                        amount={formatUnits(
-                            (balances as any)[index].result,
-                            token.decimals
-                        )}
-                        className="bg-slate-100 rounded-lg py-4 mb-2"
-                        key={token.symbol}
-                    />
-                ))}
+                tokenOptions.map((token, index) => {
+                    const balance = balances[index];
+                    if (balance.status != "success") return;
+                    return (
+                        <TokenRow
+                            token={token}
+                            amount={formatUnits(
+                                balance.result as BigNumberish,
+                                token.decimals
+                            )}
+                            className="bg-slate-100 rounded-lg py-4 mb-2"
+                            key={token.symbol}
+                        />
+                    );
+                })}
         </>
     );
 }
